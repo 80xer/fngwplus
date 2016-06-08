222c17861a0aeb07055fd0db10091669b3ae6405
leetou <leetou@fnguide.com>
2016-06-08 13:13:53 +0900

    add ga

--- app/scripts.babel/fngwplus.js
+++ app/scripts.babel/fngwplus.js
@@ -69,6 +69,15 @@ $(document).ready(() => {
 			let locpath = location.pathname
 			setViews(locationPathView[locpath])
 			stopViews(locpath)
+			var _gaq = _gaq || [];
+			_gaq.push(['_setAccount', 'UA-78966622-1']);
+			_gaq.push(['_trackPageview']);
+
+			(function() {
+			  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			  ga.src = 'https://ssl.google-analytics.com/ga.js';
+			  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			})();
 		}
 
 		function setViews(views) {
